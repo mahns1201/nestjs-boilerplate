c90f023684c9d46e98347c8e04728053da66ea5a
서민혁 <mahns1201@gmail.com>
2025-03-10 00:14:22 +0900

    Add: Nest config package

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,25 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './domain/user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import * as process from 'node:process';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `./.env`,
+    }),
     TypeOrmModule.forRootAsync({
       useFactory: () => ({
         type: 'mysql',
-        host: 'localhost',
-        port: 3306,
-        database: 'example',
-        username: 'example_user',
-        password: 'example_user_password',
+        host: process.env.MYSQL_HOST,
+        port: Number(process.env.MYSQL_PORT) || 3306,
+        database: process.env.MYSQL_DATABASE,
+        username: process.env.MYSQL_USERNAME,
+        password: process.env.MYSQL_PASSWORD,
+        autoLoadEntities: true,
+        synchronize: process.env.NODE_ENV !== 'production',
       }),
     }),
     UserModule,
